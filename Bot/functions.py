9ed85bdea933c58acabae2e07f2858a467f80ec2
sander1946 <sanderkleine2@gmail.com>
2024-09-13 15:24:46 +0200

    Revert "temperary push, has bugs"
    This reverts commit 1d91e799ac6d7b5b2a999091185cb95829d6e9aa.

--- Bot/functions.py
+++ Bot/functions.py
@@ -5,14 +5,12 @@ from discord.ext import commands
 # python imports
 from dotenv import load_dotenv
 import os
-import re
 
 # 3rd party imports
 import mysql.connector
 from mysql.connector import Error
 from mysql.connector.abstracts import MySQLConnectionAbstract
 from mysql.connector.pooling import PooledMySQLConnection
-import yt_dlp
 
 
 load_dotenv()
@@ -165,45 +163,3 @@ def delete_ticket_from_db(connection: PooledMySQLConnection | MySQLConnectionAbs
     query = "DELETE FROM open_tickets WHERE channel_id = %s"
     delete_query(connection, query, (channel_id,))
 
-
-# Function to get the video URLs from a playlist
-def get_video_urls_from_playlist(playlist_url):
-    ydl_opts: dict[str, bool] = {
-        'extract_flat': True,  # Only extract the URL, no downloads
-        'quiet': True  # Suppress output
-    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        try:
-            info_dict = ydl.extract_info(playlist_url, download=False)
-            video_urls = [entry['url'] for entry in info_dict['entries']]
-            playlist_title = info_dict['title']
-            return video_urls, playlist_title
-        except Exception as e:
-            print(f"Error: {e}")
-            return []
-
-
-# Main function
-def get_video_urls(url: str) -> list:
-    playlist_pattern = r'(?:https?://)?(?:www\.)?youtube\.com/playlist\?list=[\w-]+'
-    radio_pattern = r"^https?:\/\/(www\.)?youtube\.com\/.*[?&]list=(RD|RDEM)[^&]+.*"
-    video_pattern = r'(?:https?://)?(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/)[\w-]+'
-
-    if re.match(playlist_pattern, url):
-        video_urls, playlist_title = get_video_urls_from_playlist(url)
-        if not video_urls:
-            print("[error] No videos found or failed to fetch URLs.")
-            return []
-        return video_urls
-
-    elif re.match(radio_pattern, url):
-        print("[warning] The provided URL is a radio URL and cannot be processed.")
-        return "radio"
-
-    elif re.match(video_pattern, url):
-        return [url]
-
-    else:
-        print("[warning] The provided URL is not a valid YouTube playlist or video URL.")
-        return []
