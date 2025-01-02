0cb49a13171996cf883a4d24bea0973da1eeb695
sander1946 <71840186+sander1946@users.noreply.github.com>
2025-01-02 19:16:42 +0100

    moved the modal to a separate file

--- Bot/musicMenu.py
+++ Bot/musicMenu.py
@@ -12,6 +12,7 @@ from dotenv import load_dotenv
 import os
 
 # local imports
+from cogs.utils.BaseModal import BaseModal
 from functions import load_ids, get_video_urls
 from logger import logger
 
@@ -37,35 +38,6 @@ youtube_base_url: str = 'https://www.youtube.com/'
 youtube_results_url: str = youtube_base_url + 'results?'
 youtube_watch_url: str = youtube_base_url + 'watch?v='
 ytdl: yt_dlp.YoutubeDL = yt_dlp.YoutubeDL(yt_dlp_options)
-
-
-class BaseModal(discord.ui.Modal):
-    _interaction: discord.Interaction | None = None
-
-    # sets the interaction attribute when a valid interaction is received i.e modal is submitted
-    # via this we can know if the modal was submitted or it timed out
-    async def on_submit(self, interaction: discord.Interaction) -> None:
-        # if not responded to, defer the interaction
-        if not interaction.response.is_done():
-            await interaction.response.defer()
-        self._interaction = interaction
-        self.stop()
-
-    # make sure any errors don't get ignored
-    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
-        tb = "".join(traceback.format_exception(type(error), error, error.__traceback__))
-        message = f"An error occurred while processing the URL, Please check the URL and try again."
-        logger.error(f"An error occurred while processing the URL, Please check the URL and try again.{tb}, {error}")
-        try:
-            await interaction.response.send_message(message, ephemeral=True)
-        except discord.InteractionResponded:
-            logger.error("An error occurred while processing the URL, Please check the URL and try again.")
-        #     await interaction.edit_original_response(content=message, view=PersistentMusicView())
-        self.stop()
-
-    @property
-    def interaction(self) -> discord.Interaction | None:
-        return self._interaction
 
 
 class PersistentMusicView(discord.ui.View):
